627e9c3f01fbace0b4b013a7a96e4b4f552dd2c5
Evan Serre <evan@spiresoftware.com>
2020-12-01 17:58:31 -0800

    data cleaned and concatenated

--- parseData.py
+++ parseData.py
@@ -37,7 +37,7 @@ def findChampMasteries(lol_watcher, summonerDict):
     champData = current_champ_list['data']
     champKeys = dict()
     for champ in champData:
-        champKeys[champData[champ]['key']] = champ
+        champKeys[champData[champ]['key']] = champData[champ]['name']
 
     championMasteries = dict()
     depth = 0
@@ -71,6 +71,7 @@ def findChamps(lol_watcher):
 
     current_champ_list = lol_watcher.data_dragon.champions(champions_version)
     champData = current_champ_list['data']
+    print(current_champ_list)
     
     champNameSet = set()
     for champ in champData:
@@ -106,51 +107,79 @@ def parseRankedData(path):
                 for tempChampDataRaw in tempParse:
                     classSplit = tempChampDataRaw.split('"')
                     dataValue = classSplit[len(classSplit) - 2]
+                    # I hate Nunu in HTML
+                    if dataValue == 'Nunu &amp; Willump':
+                        dataValue = 'Nunu & Willump'
                     s10ChampDataParsedList.append(dataValue)
                 
                 s10ChampDataParsedBySummoner[s10ChampDataParsedList[0]] = {'winRate':s10ChampDataParsedList[1], 'KDA':s10ChampDataParsedList[2]}
         s10ChampDataParsed[summonerTxt[:len(summonerTxt) - 4]] = s10ChampDataParsedBySummoner
     return s10ChampDataParsed
 
+def combineOpggDataAndMastery(pathOpggData, pathMastery, pathTarget):
+    with open(pathOpggData, 'r') as parsedOPGG:
+        tempCombination = json.load(parsedOPGG)
+        with open(pathMastery, 'r') as parsedMastery:
+            masteryDict = json.load(parsedMastery)
+            with open(pathTarget, 'w') as summonerData:
+                for summonerName in masteryDict:
+                    # print(f'name: {summonerName}')
+                    for champName in masteryDict[summonerName]:
+                        tempCombination[summonerName].get(champName, dict())['mastery'] = masteryDict[summonerName][champName]
+                        # print(tempCombination)
+                json.dump(tempCombination, summonerData, indent=2)
+    
+    return pathTarget
+
+def percentizeMastery(path):
+    with open(path, 'r') as summonerDataRead:
+        tempData = json.load(summonerDataRead)
+        # print(tempData)
+        resultData = tempData.copy()
+        for summonerName in tempData:
+            totalMastery = 0
+            for champName in tempData[summonerName]:
+                print(tempData[summonerName][champName]['mastery'])
+                totalMastery += tempData[summonerName][champName]['mastery']
+            for champName in resultData[summonerName]:
+                resultData[summonerName][champName]['mastery'] = tempData[summonerName][champName]['mastery'] / totalMastery
+
+    with open(path, 'w') as summonerDataWrite:
+        json.dump(resultData, summonerDataWrite, indent=2)
+    
+    return path
+            
+
+
 # code copied from: https://www.geeksforgeeks.org/working-with-json-data-in-python/
 
 '''
-names = findSummonerList(lol_watcher)
+with open('rawSummonerData/allSummoners.json', 'r') as allSummonerNamesAndKeys:
+    summonerNames = json.load(allSummonerNamesAndKeys)
+
 champMasteriesDict = findChampMasteries(lol_watcher, names)
 
 with open('rawSummonerData/allSummoners.json', 'w') as allSummonerNames:
     json.dump(names, allSummonerNames, indent=2)
 
+
+with open('parsedSummonerData/opggDataBySummoner.json', 'w') as parsedOPGG:
+    json.dump(parseRankedData('rawSummonerData'), parsedOPGG, indent=2)
+
+combineOpggDataAndMastery('parsedSummonerData/opggDataBySummoner.json', 'parsedSummonerData/champMasteries.json', 'parsedSummonerData/summonerDataByChamp.json')
+
+
+with open('rawSummonerData/allSummoners.json', 'r') as allSummonerNamesAndKeys:
+    summonerNames = json.load(allSummonerNamesAndKeys)
+
+champMasteriesDict = findChampMasteries(lol_watcher, summonerNames)
+
 with open('parsedSummonerData/champMasteries.json', 'w') as masteryData:
     json.dump(champMasteriesDict, masteryData, indent=2)
+
+combineOpggDataAndMastery('parsedSummonerData/opggDataBySummoner.json', 'parsedSummonerData/champMasteries.json', 'parsedSummonerData/summonerDataByChamp.json')
 '''
 
-'''
+percentizeMastery('parsedSummonerData/summonerDataByChamp.json')
+
 # print(findChamps(lol_watcher))
-with open('rawSummonerData/1v9r.txt', 'r') as s10ChampDataTxt:
-    s10ChampDataRaw = s10ChampDataTxt.read()
-    s10ChampDataRawList = s10ChampDataRaw.split('<td class="ChampionImage Cell">')
-    s10ChampDataRawList.pop(0)
-
-    s10ChampDataParsed = dict()
-    relevantClasses = ['<td class="ChampionName Cell', '<td class="RatioGraph Cell', '<td class="KDA Cell']
-    for champCellRaw in s10ChampDataRawList:
-        tempParse = []
-        champCellRaw = champCellRaw.splitlines()
-        for relevantClass in relevantClasses:
-            for jsLine in champCellRaw:
-                if relevantClass in jsLine:
-                    tempParse.append(jsLine)
-                    break
-
-        s10ChampDataParsedList = []
-        for tempChampDataRaw in tempParse:
-            classSplit = tempChampDataRaw.split('"')
-            dataValue = classSplit[len(classSplit) - 2]
-            s10ChampDataParsedList.append(dataValue)
-        
-        s10ChampDataParsed[s10ChampDataParsedList[0]] = {'winRate':s10ChampDataParsedList[1], 'KDA':s10ChampDataParsedList[2]}
-    print(s10ChampDataParsed)
-'''
-with open('parsedSummonerData/champDataBySummoner.json', 'w') as parsedOPGG:
-    json.dump(parseRankedData('rawSummonerData'), parsedOPGG, indent=2)
